3f7871561a7b4c1ee92bc45c31fa3e588797feae
Manoj Paudel <manoj.765421@bumc.tu.edu.np>
2023-12-04 15:40:30 +0545

    worked on main.py file

---
+++ main.py
@@ -0,0 +1,28 @@
+import book_management
+import user_management
+import borrow_book
+
+
+while True:
+    command:str = input('Enter command [add book, view books, check book, add user, check user]').lower().replace(' ', '')
+
+    match command:
+        case 'addbook':
+            book_management.BookManagement.add_books()
+        
+        case 'viewbooks':
+            book_management.BookManagement.show_books()
+            
+        
+        case 'checkbook':
+            book_management.BookManagement.checkBook()
+        
+        case 'adduser':
+            user_management.User.addUser()
+        
+        case 'checkuser':
+            user_management.User.checkUser()
+            
+        case 'exit':
+            break 
+        
