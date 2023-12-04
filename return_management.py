2accc05f3ee8ec5b18999d70ab13bfd9c635cf59
Ranjan Thapa <98253643+sntiago1011@users.noreply.github.com>
2023-12-04 21:12:18 +0545

    return book

--- return_management.py
+++ return_management.py
@@ -1,26 +1,29 @@
 import csv
+
+
 class ReturnBook:
-    
+
     def return_book(self):
         try:
-            user_id=int(input("Enter member id:"))
-            book_id=int(input("Enter id of the book you want to return:"))
-        except Exception as E:
+            user_id = int(input("Enter member id:"))
+            book_id = int(input("Enter id of the book you want to return:"))
+        except ValueError:
             print("Error {E} Enter Number only")
-        with open('borrowed_book.csv','r') as file:
+        with open('borrowed_book.csv', 'r') as file:
             reader = csv.reader(file)
-            check_user_borrow = any((True for row in reader if row[0] == str(user_id) and row[1]==str(book_id)))
-        if check_user_borrow:
-            print(row)
-            # with open('book_inventory.csv', 'r') as file:
-            #     file_reader = csv.reader(file)
-            #     rows = list(file_reader)
-            # for row in rows:
-            #     if int(row[0]) == book_id:
-            #         row[4] = int(row[4]) + 1
-            #         break
-            # with open('book_inventory.csv', 'w', newline='') as file:
-            #     csv_writer = csv.writer(file)
-            #     csv_writer.writerows(rows)
-        else:
-            print("Enter correct member id and book id")
+            rows = list(reader)
+            row_to_delete = None
+            for i, row in enumerate(rows):
+                if row[0] == str(user_id) and row[1] == str(book_id):
+                    row_to_delete = i
+                    break
+
+            if row_to_delete is not None:
+                rows.pop(row_to_delete)
+            with open('borrowed_book.csv', 'w', newline='') as updated_file:
+                writer = csv.writer(updated_file)
+                writer.writerows(rows)
+
+                print("success")
+
+
--- user_management.py
+++ user_management.py
@@ -1,47 +1,52 @@
 import csv
+
+
 class User:
-    _user_id=0
+    _user_id = 0
+
     def __init__(self):
         print("User instance")
+
     @classmethod
     def getUser_id(cls):
-        with open('user_id.log','r') as file:
+        with open('user_id.log', 'r') as file:
             for f in file:
-                cls._user_id=int(f)
-        cls._user_id+=1
-        with open('user_id.log','w') as file:
+                cls._user_id = int(f)
+        cls._user_id += 1
+        with open('user_id.log', 'w') as file:
             file.write(str(cls._user_id))
         return cls._user_id
 
     def addUser(self):
-        full_name=input("Enter your full name:")
+        full_name = input("Enter your full name:")
         try:
-            contact_no=int(input("Enter your contact number:"))
+            contact_no = int(input("Enter your contact number:"))
         except Exception as E:
             print("Error={E}! Enter number only")
             return
-        duplicate_number=False
-        with open('user.csv','r') as file:
-            csvreader=csv.reader(file)
-            header=next(csvreader)
+        duplicate_number = False
+        with open('user.csv', 'r') as file:
+            csvreader = csv.reader(file)
+            header = next(csvreader)
             for r in csvreader:
-                if int(r[2])==contact_no:
-                    duplicate_number=True
+                if int(r[2]) == contact_no:
+                    duplicate_number = True
                     break
         if duplicate_number:
             print("Duplicate Number Found!")
         else:
-            user_id=self.getUser_id()
-            with open('user.csv','a',newline='\n') as file:
-                writer=csv.writer(file)
-                writer.writerow([user_id,full_name,contact_no])
+            user_id = self.getUser_id()
+            with open('user.csv', 'a', newline='\n') as file:
+                writer = csv.writer(file)
+                writer.writerow([user_id, full_name, contact_no])
             print("User sucessfully Created")
-    def checkUser(self,user_id):
-        with open('user.csv','r') as file:
-            csvreader=csv.reader(file)
-            header=next(csvreader)
+
+    def checkUser(self, user_id):
+        with open('user.csv', 'r') as file:
+            csvreader = csv.reader(file)
+            header = next(csvreader)
             for r in csvreader:
-                if int(r[0])==user_id:
+                if int(r[0]) == user_id:
                     return True
                     break
             return False
