9eac03b35ec4b5fc6d8e6c681bf42aadc2b4eb23
benup211 <benup211@gmail.com>
2023-12-05 19:14:52 +0545

    Added help function

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ from return_management import ReturnBook
 class LMS(BookManagement,User,BorrowBook,ReturnBook):
     def __init__(self):
         print(f"{'Library Management System':#^100}")
+    def help_msg(self):
+        print(f"{'Possible command':#^100}")
+        print("addbook->Add new book in inventory")
+        print("viewbooks->View avaliable books in Library")
+        print("adduser->register user in Library")
+        print("borrow->Borrow book from Library")
+        print("return->Return book to Library")
+        print("help->All LMS commands")
+        print("exit->Exit LMS system")
+        print(f"{'End of command':#^100}")
 lms=LMS()
 
 def run():
     while True:
-        command:str = input('Enter command [add book, view books, add user, borrow, return]:').lower().replace(' ', '')
+        command:str = input('Enter command [addbook, viewbooks, adduser, borrow, return,help,exit]:').lower().replace(' ', '')
         try: 
             match command:
                 case 'addbook':
@@ -23,6 +33,8 @@ def run():
                     lms.borrow()
                 case 'return':
                     lms.return_book()
+                case 'help':
+                    lms.help_msg()
                 case 'exit':
                     break
         except Exception as e:
